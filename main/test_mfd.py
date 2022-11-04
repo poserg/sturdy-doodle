9e0ec0f1cb8b1c5dba75b6ef0bcbd8db7fd44ae7
Sergei Popov <poserg@gmail.com>
2022-11-04 05:42:32 +0300

    Fix flake8 warnings

--- main/test_mfd.py
+++ main/test_mfd.py
@@ -3,18 +3,16 @@
 import unittest
 
 from main.mfd import MfdClient
-import logging
+# import logging
 
 
 class Mfd(unittest.TestCase):
+    # def setUp(self):
+    #   logging.basicConfig(level=logging.DEBUG)
+    #   logging.getLogger().setLevel(logging.DEBUG)
 
-
-	# def setUp(self):
-	# 	logging.basicConfig(level=logging.DEBUG)
-	# 	logging.getLogger().setLevel(logging.DEBUG)
-
-	def test_get_last_quote(self):
-		client = MfdClient()
-		date, price = client.get_last_quote(1464)
-		self.assertRegex(date, '[0-9]{8}')
-		self.assertRegex(price, '^[0-9]+(\.[0-9]+)?$')
+    def test_get_last_quote(self):
+        client = MfdClient()
+        date, price = client.get_last_quote(1464)
+        self.assertRegex(date, '[0-9]{8}')
+        self.assertRegex(price, '^[0-9]+(.[0-9]+)?$')
