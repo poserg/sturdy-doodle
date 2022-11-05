1eff8502214c42c733a7e4560e9b2921f23cbaa7
Sergei Popov <poserg@gmail.com>
2022-11-05 14:17:33 +0300

    Remove logging from test

--- clients/test_smar_lab.py
+++ clients/test_smar_lab.py
@@ -1,14 +1,9 @@
 import unittest
 from unittest.mock import patch, MagicMock
 from clients.smart_lab import SmartLabClient
-import logging
 
 
 class SmartLabTest(unittest.TestCase):
-    def setUp(self):
-        logging.basicConfig(level=logging.DEBUG)
-        logging.getLogger().setLevel(logging.DEBUG)
-
     @patch('clients.smart_lab.requests')
     def test_retrieve_bond(self, mock_requests):
         mock_response = MagicMock()
@@ -19,6 +14,5 @@ class SmartLabTest(unittest.TestCase):
 
         client = SmartLabClient()
         bond = client.retrieve_bond_info('SU26215RMFS2')
-        print(bond)
         self.assertEqual(bond.price, 99.747)
         self.assertEqual(bond.oid, 15.72)
