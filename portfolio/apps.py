f91a542b549093171cf3cb112f07c96077141337
Sergei Popov <poserg@gmail.com>
2022-11-26 06:38:00 +0300

    Load bond information at startup

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.conf import settings
 from .clients.mfd import MfdClient
+from .clients.smart_lab import SmartLabClient
 from djmoney.money import Money
 
 
@@ -11,11 +12,17 @@ class PortfolioConfig(AppConfig):
     def ready(self):
         from .models import AssetType
         try:
-            self.upgrade_asset(AssetType.STOCK, settings.STOCK_TICKERS)
+            mfd = MfdClient()
+            self.upgrade_asset(
+                AssetType.STOCK, settings.STOCK_TICKERS, mfd.get_last_quote)
+            smart_lab = SmartLabClient()
+            self.upgrade_asset(
+                AssetType.BOND, settings.BOND_TICKERS,
+                smart_lab.retrieve_bond_info)
         except Exception as e:
             print("There was an error: ", e)
 
-    def upgrade_asset(self, type, current_tickers):
+    def upgrade_asset(self, type, current_tickers, get_asset):
         from .models import Asset
         exist_tickers = list(
             map(lambda x: x.ticker, Asset.objects.filter(type=type))
@@ -25,9 +32,8 @@ class PortfolioConfig(AppConfig):
 
         if len(current_tickers) == 0:
             return
-        mfd = MfdClient()
         for i in current_tickers:
-            stock = mfd.get_last_quote(i)
-            asset = Asset(ticker=i, name=stock.name,
-                          type=type, price=Money(stock.price, 'RUB'))
+            asset_to_add = get_asset(i)
+            asset = Asset(ticker=i, name=asset_to_add.name,
+                          type=type, price=Money(asset_to_add.price, 'RUB'))
             asset.save()
