fa90239f06acac3e9fd7656498083161163bbf3c
Sergei Popov <poserg@gmail.com>
2022-12-07 05:26:25 +0300

    Create new secret key at .env

--- accounting/settings.py
+++ accounting/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import configparser
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'insecure-f(0(eag(-gw!0)1^+=1^t%w6bcl7(ifg)6bk^s@fg4(m3kh7!m'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
