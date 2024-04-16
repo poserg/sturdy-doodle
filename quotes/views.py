e45f052ca4add07f14a8254a6e0dd00f05ae7844
Sergei Popov <poserg@gmail.com>
2024-04-16 07:05:33 +0300

    View share quotes

--- quotes/views.py
+++ quotes/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from portfolio.clients.mfd import MfdWebClient
+from django.conf import settings
 
+mfd = MfdWebClient()
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    # for i in tickers:
+    stocks = []
+    for i in settings.STOCK_TICKERS:
+        stocks.append(mfd.get_last_quote(i))
+    context = {"quotes": stocks}
+    return render(request, "quotes/index.html", context)
