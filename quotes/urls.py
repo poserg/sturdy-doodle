a70b267ceea69dcd87ac5cd0b6f6a3c13851fbdb
Sergei Popov <poserg@gmail.com>
2024-04-21 06:55:50 +0300

    View bond quotes

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("stocks", views.stocks, name="stocks"),
+    path("bonds", views.bonds, name="bonds"),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from portfolio.clients.mfd import MfdWebClient
+from portfolio.clients.dohod import DohodClient
 from django.conf import settings
 
 mfd = MfdWebClient()
+dohod_client = DohodClient()
 
-def index(request):
-    # for i in tickers:
-    stocks = []
+def stocks(request):
+    quotes = []
     for i in settings.STOCK_TICKERS:
-        stocks.append(mfd.get_last_quote(i))
-    context = {"name": "Stocks", "quotes": stocks}
-    return render(request, "quotes/index.html", context)
+        quotes.append(mfd.get_last_quote(i))
+    context = {"name": "Stocks", "quotes": quotes}
+    return render(request, "quotes/stocks.html", context)
+
+def bonds(request):
+    quotes = []
+    for i in settings.BOND_TICKERS:
+        quotes.append(dohod_client.get_last_quote(i))
+    context = {"name": "Bonds", "quotes": quotes}
+    return render(request, "quotes/bonds.html", context)
