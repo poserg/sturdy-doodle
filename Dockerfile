48c631673cb854bb3086f156f43519f63073fc75
Sergei Popov <poserg@gmail.com>
2024-05-21 06:56:07 +0300

    Expose 8080 port

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN pipenv install --system --deploy --ignore-pipfile
 
 COPY . .
 
-EXPOSE 8000
+EXPOSE 8080
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8080"]
--- accounting/settings.py
+++ accounting/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'bbai00m29f7a84328djl.containers.yandexcloud.net'
+]
 
 
 # Application definition
