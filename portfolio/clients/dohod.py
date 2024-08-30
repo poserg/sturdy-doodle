aa4c8f140e3d427ccdc24053088558854c4e8df7
Sergei Popov <poserg@gmail.com>
2024-08-30 07:26:05 +0300

    Update DohodClient

--- portfolio/clients/dohod.py
+++ portfolio/clients/dohod.py
@@ -4,6 +4,7 @@ import logging
 from portfolio.clients.smart_lab import Bond
 
 logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.DEBUG)
 
 
 class DohodClient:
@@ -15,19 +16,20 @@ class DohodClient:
     ''' # noqa
 
     def __init__(self):
-        self.url = 'https://www.dohod.ru/assets/components/dohodbonds/connectorweb.php?action=info' # noqa
+        self.url = 'https://www.dohod.ru/assets/components/dohodbonds/connectorweb.php?action=getbondinfo' # noqa
 
     def get_last_quote(self, isin):
-        quote = self._get(isin)[0]
+        quote = self._get(isin)
         return Bond(
             isin,
-            quote['name'],
-            quote['last'],
-            quote['accruedint'])
+            quote['mdShortName'],
+            quote['price'],
+            quote['addInfo']["nkd"])
 
     def _get(self, isin):
-        r = requests.post(
+        logger.debug(f'ISIN: {isin}')
+        r = requests.get(
             self.url,
-            data={'customFilters[search_string]': isin})
+            params={'isin': isin})
         logger.debug(f'response = {r.text}')
         return json.loads(r.text)
