c7fe9695a95ee2e5ed4d6c65e4bde31429e61820
Sergei Popov <poserg@gmail.com>
2025-08-12 21:50:05 +0300

    Generate quotes common page by request params

--- quotes/urls.py
+++ quotes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("bonds", views.bonds, name="bonds"),
     path("futures", views.futures, name="futures"),
     path("funds", views.funds, name="funds"),
+    path("common", views.common_request, name="common"),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -10,6 +10,7 @@ from django.conf import settings
 import logging
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 mfd = MfdWebClient()
 tbank = TBankClient()
@@ -17,24 +18,33 @@ dohod_client = DohodClient()
 bcs = BCSClient()
 
 def stocks(request):
+    quotes = get_stocks(settings.STOCK_TICKERS)
+    context = {"quotes": quotes}
+    return render(request, "quotes/stocks.html", context)
+
+def get_stocks(tickers):
     quotes = []
-    for i in settings.STOCK_TICKERS:
+    for i in tickers:
         try:
             quotes.append(tbank.get_last_quote(i))
         except:
             logging.exception(f"Fail to get {i}")
-    context = {"quotes": quotes}
-    return render(request, "quotes/stocks.html", context)
+    return quotes
 
 def bonds(request):
+    quotes = get_bonds(settings.BOND_TICKERS)
+    context = {"quotes": quotes}
+    return render(request, "quotes/bonds.html", context)
+
+def get_bonds(tickers):
     quotes = []
-    for i in settings.BOND_TICKERS:
+    for i in tickers:
         try:
             quotes.append(dohod_client.get_last_quote(i))
         except:
             logging.exception(f"Fail to get {i}")
-    context = {"quotes": quotes}
-    return render(request, "quotes/bonds.html", context)
+    return quotes
+
 
 def futures(request):
     quotes = get_quotes()
@@ -42,11 +52,34 @@ def futures(request):
     return render(request, "quotes/futures.html", context)
 
 def funds(request):
+    quotes = get_funds(settings.FUND_TICKERS)
+    context = {"quotes": quotes}
+    return render(request, "quotes/funds.html", context)
+
+def get_funds(tickers):
     quotes = []
-    for i in settings.FUND_TICKERS:
+    for i in tickers:
         try:
             quotes.append(bcs.get_last_quote(i))
         except:
             logging.exception(f"Fail to get {i}")
-    context = {"quotes": quotes}
-    return render(request, "quotes/funds.html", context)
+    return quotes
+
+def common_request(request):
+    stock_tickers = request.GET.getlist("stock", '')
+    stocks = get_stocks(stock_tickers)
+    
+    bonds_ticker = request.GET.getlist("bond", '')
+    logger.debug(f"bonds_ticker = {bonds_ticker}")
+    bonds = get_bonds(bonds_ticker)
+    
+    funds_ticker = request.GET.getlist("fund", '')
+    logger.debug(f"funds_ticker = {funds_ticker}")
+    funds = get_funds(funds_ticker)
+
+    context = {
+        "stocks": stocks,
+        "bonds": bonds,
+        "funds": funds,
+    }
+    return render(request, "quotes/common.html", context)
